5a5a01b3bc4d82afd19f0f1f1aeac2ec56be3af4
chloebrns <bernasconi.chloe@gmail.com>
2024-06-14 17:29:43 +0200

    Ajout

--- Pages/formulaire.py
+++ Pages/formulaire.py
@@ -5,7 +5,7 @@ with st.form("Form1"):
     user_name = st.text_input("Entrez votre nom")
     user_firstname = st.text_input("Entrez votre prénom")
     user_age = st.select_slider("Ton âge", options=range(0,99))
-    user_etude = st.selectbox("Ton niveau d'études:", ("BAC","BAC+2","BAC+3","BAC+4","BAC+5"))
+    user_etude = st.selectbox("Ton niveau d'études", ("BAC","BAC+2","BAC+3","BAC+4","BAC+5"))
     if st.form_submit_button("Send"):
         st.write("Envoyé")
 
